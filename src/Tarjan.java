d362f12b736b782d6c63e85beef34d0140adfb7f
natiseverino <natiseverino@gmail.com>
2019-05-30 22:30:41 -0300

    arreglo de comentarios

--- src/Tarjan.java
+++ src/Tarjan.java
@@ -13,7 +13,8 @@ public class Tarjan {
     public ArrayList<HashSet<Node>> cfc(Graph graph) {
         time = 0;
         visitedTime = new HashMap<>();
-        lowTime = new HashMap<>();
+        lowTime = new HashMap<>(); //alamacena el tiempo del primer nodo del dfs que se esta ejecutando,
+                                   // es decir el minimo tiempo de descubrimiento del subarbol dfs producido
         stack = new LinkedList<>();
         onStack = new HashSet<>(); //indica si el nodo se encuentra en el stack
         visited = new HashSet<>();
@@ -40,23 +41,26 @@ public class Tarjan {
         onStack.add(nodo);
 
         for (Node ady : nodo.getAdyacentes()) {
+            //Si el adyacente no fue visitado entonces lo visito y reviso si tiene un camino de vuelta hacia el nodo padre entonces seteo su low time igual
+            //a del tiempo de visita del padre
             if (!visited.contains(ady)) {
                 makeCFC(ady);
-                //seteo el low time de nodo como el minimo entre el y el low time de ady;
-                lowTime.compute(nodo, (v, low) ->
+                //seteo el low time de nodo como el minimo entre el mismo y el low time de ady;
+                lowTime.compute(nodo, (n, low) ->
                         Math.min(low, lowTime.get(ady))
                 );
             } 
-            //reviso si ady esta en el stack y si fue visitado antes que el low time de nodo
+            //reviso si ady esta en el stack y si fue visitado antes que el low time de nod
+            //es decir, reviso si ady es la raiz del arbol dfs
             else if (onStack.contains(ady)) {
                 //seteo el low time de nodo como el minimo entre el mismo y el visitedTime de ady;
-                lowTime.compute(nodo, (v, low) -> Math.min(low, visitedTime.get(ady))
+                lowTime.compute(nodo, (n, low) -> Math.min(low, visitedTime.get(ady))
                 );
             }
         }
 
         
-        //si el low time de nodo es el mismo que el visited time entonces es el nodo de inicio de la componente fuertemente conectada
+        //si el low time de nodo es el mismo que el visited time entonces es el nodo de inicio(raiz) de la componente fuertemente conectada
         if (visitedTime.get(nodo) == lowTime.get(nodo)) {
             HashSet<Node> ComponenteFuertementeConectada = new HashSet<>();
             Node n;
